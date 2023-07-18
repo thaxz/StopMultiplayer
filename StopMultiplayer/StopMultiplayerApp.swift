ef90d21372abf110bafa3bfd326b1bae4f8022ec
thaxz <thaxzt@gmail.com>
2023-07-18 18:40:54 -0300

    first commit

---
+++ StopMultiplayer/StopMultiplayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopMultiplayerApp.swift
+//  StopMultiplayer
+//
+//  Created by thaxz on 18/07/23.
+//
+
+import SwiftUI
+
+@main
+struct StopMultiplayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
