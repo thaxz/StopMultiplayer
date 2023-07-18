656c3df62fa6ea2ad4e75e441050e58119dba134
thaxz <thaxzt@gmail.com>
2023-07-18 20:17:09 -0300

    feature(MenuView): Making UIViewRepresentable to use PencilKit

--- StopMultiplayer/Core/Game/Views/DrawingView.swift
+++ StopMultiplayer/Core/Game/Views/DrawingView.swift
@@ -6,15 +6,52 @@
 //
 
 import SwiftUI
+import PencilKit
 
-struct DrawingView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct DrawingView: UIViewRepresentable {
+    
+    class Coordinator: NSObject, PKCanvasViewDelegate {
+        var matchManager: MatchManager
+        
+        init(matchManager: MatchManager) {
+            self.matchManager = matchManager
+        }
+        
+        func canvasViewDrawingDidChange(_ canvasView: PKCanvasView) {
+            
+        }
+        
     }
+    
+    @EnvironmentObject var matchManager: MatchManager
+    @Binding var eraserEnabled: Bool
+    
+    var canvasView = PKCanvasView()
+    
+    func makeUIView(context: Context) -> PKCanvasView {
+        canvasView.drawingPolicy = .anyInput
+        canvasView.delegate = context.coordinator
+        canvasView.tool = PKInkingTool(.pen, color: .black, width: 5)
+        canvasView.isUserInteractionEnabled = matchManager.currentlyDrawing
+        
+        return canvasView
+    }
+    
+    func updateUIView(_ uiView: PKCanvasView, context: Context) {
+        canvasView.tool = eraserEnabled ? PKEraserTool(.vector) : PKInkingTool(.pen, color: .black, width: 5)
+        
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(matchManager: matchManager)
+    }
+    
 }
 
 struct DrawingView_Previews: PreviewProvider {
+    @State static var eraser: Bool = false
     static var previews: some View {
-        DrawingView()
+        DrawingView(eraserEnabled: $eraser)
+            .environmentObject(MatchManager())
     }
 }
