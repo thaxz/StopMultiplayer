0c6de436da3f237ff180e758943083a22c2fbc49
thaxz <thaxzt@gmail.com>
2023-07-18 21:40:38 -0300

    feature(MatchManager): Working on auth manager

--- StopMultiplayer/Managers/MatchManager.swift
+++ StopMultiplayer/Managers/MatchManager.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import GameKit
+import PencilKit
 
 final class MatchManager: ObservableObject {
     
@@ -25,5 +27,46 @@ final class MatchManager: ObservableObject {
     // state of auth
     @Published var authenticationState: PlayerAuthState = .authenticating
     
+    // Creating match
+    var match: GKMatch?
+    var otherPlayer: GKPlayer?
+    var localPlayer: GKLocalPlayer = GKLocalPlayer.local
+    
+    // Wich player goes first
+    var playerUUIDKey = UUID().uuidString
+    
+    // Creating scene
+    var rootViewController: UIViewController? {
+        let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+        return windowScene?.windows.first?.rootViewController
+    }
+    
+    // Authenticating user
+    func authenticateUser(){
+        // se tiver viewcontroller
+        GKLocalPlayer.local.authenticateHandler = { [self] viewController, error in
+            if let viewController = viewController {
+                rootViewController?.present(viewController, animated: true)
+                return
+            }
+            // se tiver erro
+            if let error = error {
+                authenticationState = .error
+                print(error.localizedDescription)
+                return
+            }
+            // se tiver autenticado
+            if localPlayer.isAuthenticated {
+                if localPlayer.isMultiplayerGamingRestricted {
+                    authenticationState = .restricted
+                } else {
+                    // autenticado
+                    authenticationState = .authenticaded
+                }
+            } else {
+                authenticationState = .unauthenticated
+            }
+        }
+    }
     
 }
